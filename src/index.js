199be617802d8a18610246583712396b0a34da50
saurabh <saurabhgrewak@gmail.com>
2019-04-14 01:34:04 +0530

    14 april redux remove from index file

--- src/index.js
+++ src/index.js
@@ -10,25 +10,8 @@ import { logger } from 'redux-logger';
 import root from './saga/index';
 import allfiles from './reducers/allfiles';
 
-// create the saga middleware
-const sagaMiddleware = createSagaMiddleware();
 
-// dev tools middleware
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-  // create a redux store with our reducer above and middleware
-  let store = createStore(
-    allfiles,
-    compose(applyMiddleware(sagaMiddleware), reduxDevTools)
-  );
-
-console.log(store.getState());
-
-sagaMiddleware.run(root);
-
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
