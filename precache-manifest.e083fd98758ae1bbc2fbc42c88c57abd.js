f9149c6d577ab9c9a0cc28ccb717c849e72180f6
saurabh <saurabhgrewak@gmail.com>
2019-05-24 00:11:01 +0530

    Updates

---
+++ precache-manifest.e083fd98758ae1bbc2fbc42c88c57abd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ab51e0edb42cea523edbc4da4c9b9a6",
+    "url": "/Traduate-Frontend/static/media/fruit.4ab51e0e.gif"
+  },
+  {
+    "revision": "16d4317a4eb814f54fe0",
+    "url": "/Traduate-Frontend/static/js/runtime~main.9aea846b.js"
+  },
+  {
+    "revision": "4dfce1cfd3462cd2c83b",
+    "url": "/Traduate-Frontend/static/js/main.163f0f53.chunk.js"
+  },
+  {
+    "revision": "3b9f9dd741f2c021b084",
+    "url": "/Traduate-Frontend/static/js/2.60a9678d.chunk.js"
+  },
+  {
+    "revision": "4dfce1cfd3462cd2c83b",
+    "url": "/Traduate-Frontend/static/css/main.06259d85.chunk.css"
+  },
+  {
+    "revision": "3b9f9dd741f2c021b084",
+    "url": "/Traduate-Frontend/static/css/2.09f68945.chunk.css"
+  },
+  {
+    "revision": "a2e0613a962ac25e32ceffabd1d57a4a",
+    "url": "/Traduate-Frontend/index.html"
+  }
+];
